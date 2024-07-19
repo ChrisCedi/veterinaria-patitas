3211ad37f200d728e7bdacacdf3074ab94edbd47
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-07-19 16:01:39 -0600

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <div>Hola a todos</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/presentation/navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+
+export const Navigation = () => {
+  return (
+    <Router>
+      <Routes>{}</Routes>
+    </Router>
+  );
+};

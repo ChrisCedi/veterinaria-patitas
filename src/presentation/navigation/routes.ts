0cc6f7d417a794c1e76e6049fec026087e3426f5
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-07-19 18:23:44 -0600

    navigation in app with home screen

--- src/presentation/navigation/routes.ts
+++ src/presentation/navigation/routes.ts
@@ -1,1 +1,25 @@
-export const routes = [];
+import { MainLayout } from "../layouts/MainLayout";
+import { Home } from "../screens/Home/Home";
+import { FC } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface RoutesProps {
+  id: string;
+  path: string;
+  layout: FC<Props>;
+  component: FC;
+  exact: boolean;
+}
+
+export const routes: RoutesProps[] = [
+  {
+    id: "home-id",
+    path: "/",
+    layout: MainLayout,
+    component: Home,
+    exact: true,
+  },
+];
